d15722d76875a51712e462e04c8c787098ac7a37
Patricia Varadi <patricia_varadi@yahoo.com>
2020-10-08 23:56:14 +0300

    Moore's Law assignment

---
+++ Moore's Law/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moore_s_Law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            float x = Convert.ToInt32((Math.Log(n, 2)));
+            double y = x * 1.5;            
+            Console.WriteLine("Dupa {0} ani va fi puterea de calcul de {1} ori mai mare.", y, n);
+            Console.ReadKey();
+        }
+    }
+}
